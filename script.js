afdd30c9479f8f1cae47d1fdd3d7b1f15f810590
Durotolu <40144482+Duroak@users.noreply.github.com>
2019-08-19 16:18:42 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+let searchUrl='https://developer.github.com/users/:username/repos'
+
+function formatQuery(parameters) { 
+    const queryItems = Object.keys(parameters).map(key => `${key}=${parameters[key]}`)
+    return queryItems.join('&');
+}
+
+function results(responseJson, maxResults) {
+    console.log(responseJson);
+    $('#result-list').empty();
+
+}
+
+function getProfile(query, maxResults=10) {
+    const parameters = {
+        q: query,
+        type: all,
+        sort: full_name,
+        direction: asc,
+    }
+
+    const queryUrl = formatQuery(parameters)
+    
+    const url = searchUrl + '?' + queryUrl;  
+    
+    console.log(url);
+
+    fetch(url)
+        .then(response => response.json())
+        .then(responseJson => console.log(responseJson))
+}
+
+function submitForm() {
+    $('#js-form').submit(event => {
+        event.preventDefault();
+        const searchName = $('#js-search-profile').val();
+        const maxResults = ('#js-max-results').val();
+        getProfile(searchName, maxResults);
+    })
+}
+
+$(submitForm);
